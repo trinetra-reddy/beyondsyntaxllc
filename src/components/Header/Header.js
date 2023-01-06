ef295e6e577b2c7def8b766c47946de2f3d90cc3
Trinethra <trinethraalamur@gmail.com>
2023-01-06 23:57:21 +0530

    Updated few colors

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './Header.scss';
+import Logo from '../../images/logo.png';
 
 const Header = () => {
     return (
         <header>
         <nav className="navbar">
-          <a href="header.html" className="logo">BEYONDSYNTAXLLC.COM</a>
+          <Link to="/" className="logo">
+            <img src={Logo} alt="Logo"/>
+          </Link>
           <div className="navlink">
             <ul>
-                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/">Home</Link></li>
                 <li><Link to="/about-us">About Us</Link></li>
-                <li><Link to="/it-staffing">It Staffing</Link></li>
+                <li><Link to="/it-staffing">IT Staffing</Link></li>
                 <li><Link to="/contact-us">Contact us</Link></li>
             </ul>
           </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const CONSTANTS = {
     // your constants goes here...
     ROUTES: {
-        HOME_PAGE: '/home',
+        HOME_PAGE: '/',
         CONTACT_PAGE: '/contact-us',
         ABOUT_PAGE: '/about-us',
         ITSTAFFING_PAGE: '/it-staffing',
