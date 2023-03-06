718bd8d8e1da2714e99d239a351f4b266707dbed
Raavi Haritha <haritha4682@gmail.com>
2023-03-06 20:02:40 +0530

    IMAGE COMPONENT ADDED

---
+++ src/components/ImageTextComponent/ImageTextComponent.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./ImageTextComponent.scss";
+import { Link } from "react-router-dom";
+
+const ImageTextComponent = (props) => {
+  return (
+    <div className={`imagetextcomponent ${props.imgposition}`}>
+      <div className="imagetextcomponent-image">
+        <img src={props.image} alt="image" />
+      </div>
+      <div className="imagetextcomponent-content">
+        <div className="imagetextcomponent-content-heading">
+          {props.heading}
+        </div>
+        <hr className="imagetextcomponent-heading-ul"></hr>
+        <div className="imagetextcomponent-content-paragraph">
+          {props.description}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageTextComponent;
