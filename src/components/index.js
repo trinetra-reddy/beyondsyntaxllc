992e1ddda50211c570bdabd56fedc3d46dd95c84
AmruthaAnumula <ammuamrutha35423@gmail.com>
2023-03-08 18:29:50 +0530

    Changes are done

--- src/components/index.js
+++ src/components/index.js
@@ -9,11 +9,6 @@ import Ourtechnologies from "./Ourtechnologies/Ourtechnologies";
 import Ourclients from "./Ourclients/Ourclients";
 import Aboutbanner from "./Aboutusbanner/Aboutusbanner";
 import Whypartnerwithus from "./Whypartnerwithus/Whypartnerwithus";
-import Itstaffingcontent from "./Itstaffingcontent/Itstaffingcontent";
-import Weoffer from "./Weoffer/Weoffer";
-import RpoMps from "./RpoMps/RpoMps";
-import Visionmission from "./Visionmission/Visionmission";
-import StaffingSolutions from "./StafffingSolutions/StaffingSolutions";
 import Contactusbanner from "./Contactusbanner/Contactusbanner";
 import Contactuscontent from "./Contactuscontent/Contactuscontent";
 import Contactusform from "./Contactusform/Contactusform";
@@ -22,8 +17,8 @@ import BannerComponent from "./BannerComponent/BannerComponent";
 import TextComponent from "./TextComponent/TextComponent";
 import ItStaffingStreams from "./ItStaffingStreams/ItStaffingStreams";
 import MissionVision from "./MissionVision/MissionVision";
-
+import StaffingSolutions from "./StafffingSolutions/StafffingSolutions";
 import ItStaffingStreamsWrapper from "./ItStaffingStreams/ItStaffingStreamsWrapper";
 import ItStaffingOutSourcing from "./ItStaffingStreams/ItStaffingOutSourcing";
 
-export { Header, Footer,ImageTextComponent,MissionVision,ItStaffingStreams,ItStaffingOutSourcing,ItStaffingStreamsWrapper,BannerComponent,TextComponent, Imagecarousel, Ourservices, Aboutbeyondsyntax, Whywithus, Howcanwehelpyou, Ourtechnologies, Ourclients, Aboutbanner, Whypartnerwithus, Itstaffingcontent, Weoffer, RpoMps, Contactusbanner, Contactuscontent, Contactusform};
+export { Header, Footer,ImageTextComponent,MissionVision,StaffingSolutions,ItStaffingStreams,ItStaffingOutSourcing,ItStaffingStreamsWrapper,BannerComponent,TextComponent, Imagecarousel, Ourservices, Aboutbeyondsyntax, Whywithus, Howcanwehelpyou, Ourtechnologies, Ourclients, Aboutbanner, Whypartnerwithus, Contactusbanner, Contactuscontent, Contactusform};
